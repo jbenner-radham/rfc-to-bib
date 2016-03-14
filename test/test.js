9438f6e01fab0f543d490fec275296918f1a7acc
James Benner <james.benner@gmail.com>
2016-03-14 19:27:42 -0400

    Added 'should parse "176" from "[Page 176]"' test.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@
 let expect = require('chai').expect;
 
 describe('getAuthorName', () => {
-    let getAuthorName = require('../src/lib/get-author-name');
+    let getAuthorName     = require('../src/lib/get-author-name');
+    let getLastPageNumber = require('../src/lib/get-last-page-number');
 
     it('should return a string from of an array of authors', () => {
         let authors = [
@@ -21,5 +22,11 @@ describe('getAuthorName', () => {
         let control = 'J. Benner';
 
         expect(getAuthorName(author)).to.equal(control);
+    });
+
+    it('should parse "176" from "[Page 176]"', () => {
+        let page = '[Page 176]';
+
+        expect(getLastPageNumber(page)).to.equal(176);
     });
 });
