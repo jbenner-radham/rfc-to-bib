af4fefd4825c758bf5885d33a850ae33f1e3ad5b
James Benner <james.benner@gmail.com>
2017-04-28 19:11:14 -0400

    Refactoring architecture.

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-require('./lib');
+'use strict';
+
+const app = require('./lib');
+
+const FAILURE = 1;
+
+if (!process.argv[2]) {
+    console.error('Please provide an RFC number as an argument.');
+    process.exit(FAILURE);
+}
+
+let rfc = process.argv[2];
+
+app(rfc);
